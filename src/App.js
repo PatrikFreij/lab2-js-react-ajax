ac88bf9e65370c5e5780163d97d0193dc625d3c4
AndreasNed <andreas.nedbal@gmail.com>
2019-04-04 16:09:58 +0200

    Added error handling for addBook, delBook, checkoutBook

--- src/App.js
+++ src/App.js
@@ -1,58 +1,97 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Header from './components/ui/Header/Header'
 import Library from './components/Library';
 import AddBook from './components/AddBook';
 
 const apiKey = 'kJ0Ha';
+let count = 0;
+
 
 class App extends Component {
 
     state = {
-        books: [
-        ]
+        books: []
     }
-
+    //Api nyckel: kJ0Ha
+    componentDidMount = () => {
+        this.selectBooks();
+    }
     addBook = (title, author) => {
         const newBook = {
             title,
             author
         };
-
-        fetch(`https://www.forverkliga.se/JavaScript/api/crud.php?op=insert&key=${apiKey}&title=${newBook.title}&author=${newBook.author}`, {
-            method: "POST",
-            mode: "cors", // no-cors, cors, *same-origin
-        }).then((response) => console.log(response));
+        fetch(`https://www.forverkliga.se/JavaScript/api/crud.php?op=insert&key=${apiKey}&title=${newBook.title}&author=${newBook.author}`)
+            .then(resp => resp.json())
+            .then((data) => {
+                if (data.status !== "success" && count <= 10) {
+                    count++;
+                    this.addBook(title, author);
+                }
+                count = 0;
+                this.selectBooks();
+            });
     }
 
     delBook = (id) => {
-        this.setState({ books:
-                [...this.state.books.filter(book => book.id !== id)]})
+        fetch(`https://www.forverkliga.se/JavaScript/api/crud.php?op=delete&key=${apiKey}&id=${id}`)
+            .then(resp => resp.json())
+            .then((data) => {
+                if (data.status !== "success" && count <= 10) {
+                    count++;
+                    this.delBook(id);
+                }
+                count = 0;
+                this.selectBooks();
+            });
     }
 
-    //Api nyckel: kJ0Ha
-    componentDidMount(){
-        //Select Books
-        fetch('https://www.forverkliga.se/JavaScript/api/crud.php?op=select&key=' + apiKey)
-            .then(response => response.json()
-                .then(data => data.status === 'success' ? this.setState({books: data.data}) : 1 )
-                .catch(error => console.log(error)));
-    }
+    selectBooks = () => {
+        fetch(`https://www.forverkliga.se/JavaScript/api/crud.php?op=select&key=${apiKey}`)
+            .then(response => response.json())
+            .then((data) => {
+                if (data.status !== "success" && count <= 10) {
+                    count++;
+                    this.selectBooks();
+                }
+                count = 0;
+                this.setState({ books: data.data });
+
+            });
+    };
+    /*
+        checkError = (operation, data) => {
+            if (data.status !== "success") {
+                switch(operation){
+                    case "delete":
+                        this.delBook(data.data.id);
+                        break;
+                    case "add":
+                        this.addBook()
+                }
+                console.log("Failed to fetch data. Reason: " + data.message);
+                count++;
+                this.selectBooks();
+            } else {
+                console.log();
+                this.setState({books:data.data});
+            }
+        }*/
 
     render() {
-        console.log(this.state.books);
         return (
             <div className="App">
-                <Header/>
+                <Header />
                 <div className="container">
                     <div className="row form-section">
-                        <AddBook addBook={this.addBook}/>
+                        <AddBook addBook={this.addBook} />
                     </div>
                 </div>
                 <div className="display-books">
                     <div className="container">
                         <div className="col-12">
                             <ul className="list-group">
-                                <Library delBook={this.delBook} books={this.state.books}/>
+                                <Library delBook={this.delBook} books={this.state.books} />
                             </ul>
                         </div>
                     </div>
